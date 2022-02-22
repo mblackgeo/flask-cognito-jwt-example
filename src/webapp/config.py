440774faae6a3be594037eb339589e038fe37a5d
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-02-22 13:49:27 +0000

    Fix incorrect path to .env

--- src/webapp/config.py
+++ src/webapp/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 # Load variables from .env
 basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, "..", ".env"))
+load_dotenv(path.join(basedir, "..", "..", ".env"))
 
 
 class Config:
