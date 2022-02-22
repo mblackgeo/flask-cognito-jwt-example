66040000dc15c00c87ff6c66745cdf447f7adac9
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-02-22 14:43:41 +0000

    Update env with a separate env var for the site URL

--- src/webapp/config.py
+++ src/webapp/config.py
@@ -13,19 +13,22 @@ class Config:
 
     # General Config
     SECRET_KEY = environ.get("SECRET_KEY", urandom(32))
-    FLASK_APP = environ.get("FLASK_APP")
-    FLASK_ENV = environ.get("FLASK_ENV")
+    FLASK_APP = environ["FLASK_APP"]
+    FLASK_ENV = environ["FLASK_ENV"]
+    FLASK_SITE_URL = environ["FLASK_SITE_URL"]
 
-    AWS_DEFAULT_REGION = environ.get("AWS_DEFAULT_REGION")
-    AWS_COGNITO_DOMAIN = environ.get("AWS_COGNITO_DOMAIN")
-    AWS_COGNITO_USER_POOL_ID = environ.get("AWS_COGNITO_USER_POOL_ID")
-    AWS_COGNITO_USER_POOL_CLIENT_ID = environ.get("AWS_COGNITO_USER_POOL_CLIENT_ID")
-    AWS_COGNITO_USER_POOL_CLIENT_SECRET = environ.get(
-        "AWS_COGNITO_USER_POOL_CLIENT_SECRET"
-    )
-    AWS_COGNITO_REDIRECT_URL = environ.get("AWS_COGNITO_REDIRECT_URL")
+    # Cognito
+    AWS_DEFAULT_REGION = environ["AWS_DEFAULT_REGION"]
+    AWS_COGNITO_DOMAIN = environ["AWS_COGNITO_DOMAIN"]
+    AWS_COGNITO_USER_POOL_ID = environ["AWS_COGNITO_USER_POOL_ID"]
+    AWS_COGNITO_USER_POOL_CLIENT_ID = environ["AWS_COGNITO_USER_POOL_CLIENT_ID"]
+    AWS_COGNITO_USER_POOL_CLIENT_SECRET = environ["AWS_COGNITO_USER_POOL_CLIENT_SECRET"]
+    AWS_COGNITO_REDIRECT_URL = path.join(FLASK_SITE_URL, "postlogin")
 
+    # JWT
     JWT_ALGORITHM = "RS256"
+    # JWT_TOKEN_LOCATION = ["cookies"]
+    # JWT_COOKIE_SECURE = FLASK_ENV == "prod"  # True in prod
 
     # Static Assets
     STATIC_FOLDER = "static"
