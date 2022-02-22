0c09cb93855bb760dbeb841970e90c7017f1ac4a
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-02-22 15:22:51 +0000

    Implement tests for additional routes

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,15 +1,19 @@
-from webapp import __version__
-
-
-def test_version():
-    assert __version__ == "0.1.0"
-
-
 def test_health(client):
     response = client.get("/health")
     assert response.status_code == 200
+    assert response.json == {"status": "ok"}
 
 
 def test_home(client):
     response = client.get("/")
     assert response.status_code == 200
+    assert "Welcome to the sample app" in str(response.data)
+    assert "<title>Welcome!</title>" in str(response.data)
+
+
+def test_private_mocked(client, mocker):
+    mocker.patch("flask_jwt_extended.view_decorators.verify_jwt_in_request")
+    response = client.get("/private")
+    assert response.status_code == 200
+    assert "<title>This is a protected page!</title>" in str(response.data)
+    assert "Well done, you have authenticated successfully" in str(response.data)
