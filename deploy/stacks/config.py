4ab5796436b222f8bd3b2c43420db8800dc50b2f
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-02-23 13:26:24 +0000

    Fix configuration to correctly load from parent .env file

--- deploy/stacks/config.py
+++ deploy/stacks/config.py
@@ -1,22 +1,22 @@
-from os.path import join
+from os.path import abspath, dirname, join
 from typing import Optional
 
 from pydantic import BaseSettings, Field
 
 
-class Conf(BaseSettings):
-    ENV: str = Field("prod")
+class CDKConfig(BaseSettings):
+    ENV: str = Field(default="prod")
     AWS_REGION: str = Field(default="eu-west-1")
     AWS_ACCOUNT: str = Field(...)
-    AWS_DOMAIN_NAME: Optional[str] = Field(...)
-    AWS_API_SUBDOMAIN: Optional[str] = Field(...)
-    AWS_COGNITO_SUBDOMAIN: Optional[str] = Field(...)
+    AWS_DOMAIN_NAME: Optional[str] = Field(None)
+    AWS_API_SUBDOMAIN: Optional[str] = Field(None)
+    AWS_COGNITO_SUBDOMAIN: Optional[str] = Field(None)
 
     class Config:
         env_prefix = ""
         case_sentive = False
-        env_file = join(".." "..", ".env")
+        env_file = abspath(join(dirname(__file__), "..", "..", ".env"))
         env_file_encoding = "utf-8"
 
 
-cfg = Conf()
+cfg = CDKConfig()
