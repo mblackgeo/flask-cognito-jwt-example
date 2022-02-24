6972fdafe35c0022e292bb23f1bd1c0ce2c7b03e
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-02-24 13:58:07 +0000

    Setup the Lambda env vars using SSM, ensure domain name is optional

--- deploy/stacks/lambda_api_stack.py
+++ deploy/stacks/lambda_api_stack.py
@@ -4,6 +4,7 @@ from aws_cdk import aws_certificatemanager as acm
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import aws_route53 as route53
 from aws_cdk import aws_route53_targets as targets
+from aws_cdk import aws_ssm as ssm
 from constructs import Construct
 
 from stacks.config import cfg
@@ -13,11 +14,42 @@ class LambdaApiStack(core.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Stage configuration
-        opts = apigw.StageOptions(stage_name=cfg.ENV)
+        # Setup the environment variables for Lambda
+        environment = {
+            "FLASK_APP": cfg.NAMESPACE,
+            "FLASK_ENV": cfg.ENV,
+            "FLASK_SITE_URL": self.from_ssm(f"/{cfg.NAMESPACE}/api-gateway-url"),
+            "AWS_REGION": cfg.AWS_REGION,
+            "AWS_DEFAULT_REGION": cfg.AWS_REGION,
+            "AWS_COGNITO_DOMAIN": self.from_ssm(
+                f"/{cfg.NAMESPACE}/cognito-user-pool-url"
+            ),
+            "AWS_COGNITO_USER_POOL_ID": self.from_ssm(
+                f"/{cfg.NAMESPACE}/cognito-user-pool-id"
+            ),
+            "AWS_COGNITO_USER_POOL_CLIENT_ID": self.from_ssm(
+                f"/{cfg.NAMESPACE}/cognito-client-id"
+            ),
+            "AWS_COGNITO_USER_POOL_CLIENT_SECRET": self.from_ssm(
+                f"/{cfg.NAMESPACE}/cognito-client-secret"
+            ),
+        }
+
+        # Register and build an Lambda docker image
+        # This picks up on Dockerfile in the parent folder
+        fn = _lambda.DockerImageFunction(
+            self,
+            f"{construct_id}-lambda-handler",
+            code=_lambda.DockerImageCode.from_image_asset(
+                directory="..", file="Dockerfile"
+            ),
+            timeout=core.Duration.seconds(15),
+            environment=environment,
+        )
 
         # Optional Route 53 setup domain and certifcates
-        if cfg.AWS_DOMAIN_NAME:
+        domain_opts = None
+        if cfg.AWS_DOMAIN_NAME and cfg.AWS_API_SUBDOMAIN:
             root_domain = cfg.AWS_DOMAIN_NAME
             api_domain = f"{cfg.AWS_API_SUBDOMAIN}.{root_domain}"
 
@@ -39,35 +71,12 @@ class LambdaApiStack(core.Stack):
                 certificate=cert, domain_name=api_domain
             )
 
-        # Register and build an Lambda docker image
-        # This picks up on Dockerfile in the parent folder
-        fn = _lambda.DockerImageFunction(
-            self,
-            f"{construct_id}-lambda-handler",
-            code=_lambda.DockerImageCode.from_image_asset(
-                directory="..", file="Dockerfile.aws"
-            ),
-            timeout=core.Duration.seconds(15),
-            environment={
-                "FLASK_APP": "webapp",
-                "FLASK_ENV": cfg.ENV,
-                "FLASK_JWT_SECRET_KEY": "",  # TODO generate this in the app
-                "FLASK_SITE_URL": "",  # from API gateway or custom domain
-                "AWS_REGION": cfg.AWS_REGION,
-                "AWS_DEFAULT_REGION": cfg.AWS_REGION,
-                "AWS_COGNITO_DOMAIN": "",  # from ssm
-                "AWS_COGNITO_USER_POOL_ID": "",  # from ssm
-                "AWS_COGNITO_USER_POOL_CLIENT_ID": "",  # from ssm
-                "AWS_COGNITO_USER_POOL_CLIENT_SECRET": "",  # from ssm
-            },
-        )
-
         # Create a Lambda based rest API with optional domain name
         api = apigw.LambdaRestApi(
             self,
             f"{construct_id}-api-route",
             handler=fn,
-            deploy_options=opts,
+            deploy_options=apigw.StageOptions(stage_name=cfg.ENV),
             domain_name=domain_opts,
         )
 
@@ -80,3 +89,15 @@ class LambdaApiStack(core.Stack):
                 zone=zone,
                 target=route53.RecordTarget.from_alias(targets.ApiGateway(api)),
             )
+
+        # Export the API Gateway address to SSM
+        ssm.StringParameter(
+            self,
+            f"{construct_id}-ssm-client-id",
+            parameter_name=f"/{cfg.NAMESPACE}/api-gateway-url",
+            string_value=api.url,
+            description="API Gateway URL",
+        )
+
+    def from_ssm(self, key: str) -> str:
+        return ssm.StringParameter.value_for_string_parameter(self, key)
