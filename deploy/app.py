90052103d320b566859cb3a262f9279d2e842d89
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-02-24 13:58:22 +0000

    Enable the Lambda API stack

--- deploy/app.py
+++ deploy/app.py
@@ -2,14 +2,13 @@ import aws_cdk as cdk
 
 from stacks.cognito_stack import CognitoStack
 from stacks.config import cfg
-
-# from stacks.lambda_api_stack import LambdaApiStack
+from stacks.lambda_api_stack import LambdaApiStack
 
 app = cdk.App()
-cdk_environment = cdk.Environment(region=cfg.AWS_REGION, account=cfg.AWS_ACCOUNT)
+cdk_env = cdk.Environment(region=cfg.AWS_REGION, account=cfg.AWS_ACCOUNT)
 
-cognito = CognitoStack(app, "cognito", env=cdk_environment)
-# lambda_api = LambdaApiStack(app, "lambda-api", env=cdk_environment))
-# lambda_api.add_dependency(cognito)
+cognito = CognitoStack(app, "cognito", env=cdk_env)
+lambda_api = LambdaApiStack(app, "lambda-api", env=cdk_env)
+lambda_api.add_dependency(cognito)
 
 app.synth()
