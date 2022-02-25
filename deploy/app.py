1ed7e8ee7ff825a1ddb31e7939eaeeb377644357
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-02-25 13:39:31 +0000

    Add dependency so that Cognito is deployed before the API

--- deploy/app.py
+++ deploy/app.py
@@ -9,5 +9,6 @@ cdk_env = cdk.Environment(region=cfg.AWS_REGION, account=cfg.AWS_ACCOUNT)
 
 cognito = CognitoStack(app, "cognito", env=cdk_env)
 api_gw = ApiStack(app, "api", env=cdk_env)
+api_gw.add_dependency(cognito)
 
 app.synth()
