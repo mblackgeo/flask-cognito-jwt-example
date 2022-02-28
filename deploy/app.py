8ac3d9c4d37ab36e1827df89dc4c75fa8b15abe8
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-02-28 13:13:17 +0000

    Refactor the main app to avoid circular dependencies, add more notes around the stack order

--- deploy/app.py
+++ deploy/app.py
@@ -4,24 +4,37 @@ from stacks.api_stack import ApiStack
 from stacks.cognito_client_stack import CognitoClientStack
 from stacks.cognito_stack import CognitoStack
 from stacks.config import cfg
-from stacks.lambda_stack import LambdaStack
+from stacks.lambda_env_stack import LambdaEnvStack
 
 app = cdk.App()
 cdk_env = cdk.Environment(region=cfg.AWS_REGION, account=cfg.AWS_ACCOUNT)
 
-# Create the Cognito User Pool and API Gateway proxy to Lambda
-cognito = CognitoStack(app, "cognito", env=cdk_env)
-api = ApiStack(app, "api", env=cdk_env)
-api.add_dependency(cognito)
+# Create the API Gateway and User Pools up front. Later we add the client
+# and register the Lambda target on the API
+api = ApiStack(scope=app, construct_id="api", env=cdk_env)
+cognito = CognitoStack(scope=app, construct_id="cognito", env=cdk_env)
 
-# Register a client to the Cognito user pool, after both are already setup
-# This is so we can resolve the URL of the API gateway after it has been
-# created
-client = CognitoClientStack(app, "client", user_pool=cognito.user_pool, env=cdk_env)
-client.add_dependency(cognito)
+# Register a client to the Cognito user pool, after both API and User Pool
+# are already setup. This is so we can resolve the URL of the API gateway
+# after it has been created and correctly register the post login URL on
+# the user pool client
+client = CognitoClientStack(
+    scope=app,
+    construct_id="cognito-client",
+    env=cdk_env,
+)
 client.add_dependency(api)
+client.add_dependency(cognito)
 
-# Register the lambda function to the API gateway
-lambda_fn = LambdaStack(app, "lambda", http_api=api.http_api)
+# Finally we can populate the Lambda function with the required environment
+# variables from all the above stacks
+# An alternative option here would be to resolve the parameters from SSM at runtime
+# rather than at deployment time
+lambda_env = LambdaEnvStack(
+    scope=app,
+    construct_id="lambda-env",
+    env=cdk_env,
+)
+lambda_env.add_dependency(client)
 
 app.synth()
