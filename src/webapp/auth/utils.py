c89067eb112ea50b6abc334eef8aeb5f9e013cd1
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-01 14:57:55 +0000

    Add a login required decorator that will redirect to Cognito if no cookie is present

--- src/webapp/auth/utils.py
+++ src/webapp/auth/utils.py
@@ -1,7 +1,11 @@
 import json
 import logging
+from functools import wraps
 
 import requests
+from flask import current_app, redirect
+from flask_awscognito import AWSCognitoAuthentication
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
 from urllib3.exceptions import LocationParseError
 
 
@@ -14,3 +18,21 @@ def get_cognito_public_keys(region: str, pool_id: str) -> str:
 
     except (KeyError, LocationParseError, requests.exceptions.ConnectionError):
         logging.warning("Could not load public keys from Cognito User Pool")
+
+
+def login_required():
+    """If a user is not logged, redirect them to the Cognito UI"""
+
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request(optional=True)
+            if get_jwt_identity():
+                return fn(*args, **kwargs)
+            else:
+                aws_auth = AWSCognitoAuthentication(current_app)
+                return redirect(aws_auth.get_sign_in_url())
+
+        return decorator
+
+    return wrapper
