894d6f1c64cf9e491032d9338881aeefc8b14936
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-24 15:46:44 +0000

    Simplify auth routes

--- src/webapp/auth/routes.py
+++ src/webapp/auth/routes.py
@@ -1,31 +1,29 @@
-from flask import Blueprint, current_app, make_response, redirect, request, url_for
-from flask_awscognito import AWSCognitoAuthentication
-from flask_jwt_extended import set_access_cookies, unset_jwt_cookies
-
-aws_auth = AWSCognitoAuthentication(current_app)
-
+from flask import Blueprint, redirect, url_for
+from flask_cognito_lib.decorators import (
+    cognito_login,
+    cognito_login_callback,
+    cognito_logout,
+)
 
 bp = Blueprint("auth_bp", __name__)
 
 
 @bp.route("/login", methods=["GET", "POST"])
+@cognito_login
 def login():
     """Redirect to the Cognito Hosted UI for login"""
-    return redirect(aws_auth.get_sign_in_url())
+    pass
 
 
 @bp.route("/postlogin", methods=["GET"])
+@cognito_login_callback
 def postlogin():
     """After a successful login store the access token as a cookie and redirect"""
-    access_token = aws_auth.get_access_token(request.args)
-    resp = make_response(redirect(url_for("private_bp.private")))
-    set_access_cookies(resp, access_token, max_age=30 * 60)  # 30 mins
-    return resp
+    return
 
 
 @bp.route("/logout", methods=["GET", "POST"])
+@cognito_logout
 def logout():
     """Logout the user and delete their JWT cookie"""
-    resp = make_response(redirect(url_for("home_bp.home")))
-    unset_jwt_cookies(resp)
-    return resp
+    return redirect(url_for("home_bp.home"))
