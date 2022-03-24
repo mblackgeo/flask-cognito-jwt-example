bb280f86305f9b1a79a204e983a4d21a65f60ef7
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-03-24 15:46:23 +0000

    Remove JWTManager

--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -1,12 +1,6 @@
-import logging
-
 from flask import Flask
+from flask_cognito_lib import CognitoAuth
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager
-from jwt.algorithms import RSAAlgorithm
-from jwt.exceptions import InvalidKeyError
-
-from webapp.auth.utils import get_cognito_public_keys
 
 __version__ = "0.1.0"
 __all__ = ["__version__", "create_app"]
@@ -20,23 +14,8 @@ def create_app() -> Flask:
     # Setup CORS middleware
     CORS(app)
 
-    # Setup JWT
-    try:
-        app.config["JWT_PUBLIC_KEY"] = RSAAlgorithm.from_jwk(
-            get_cognito_public_keys(
-                region=app.config["AWS_REGION"],
-                pool_id=app.config["AWS_COGNITO_USER_POOL_ID"],
-            )
-        )
-    except InvalidKeyError:
-        logging.warning(
-            (
-                "Could not verify Cognito public keys. "
-                "Secure routes may not be accessible"
-            )
-        )
-
-    JWTManager(app)
+    # Setup Cognito auth
+    CognitoAuth(app)
 
     with app.app_context():
         from .auth import routes as auth_routes  # noqa: F401
--- src/webapp/config.py
+++ src/webapp/config.py
@@ -1,12 +1,7 @@
 """App configuration."""
-import logging
 from os import environ, path, urandom
 
 from dotenv import load_dotenv
-from jwt.algorithms import RSAAlgorithm
-from jwt.exceptions import InvalidKeyError
-
-from webapp.auth.utils import get_cognito_public_keys
 
 # Load variables from .env
 basedir = path.abspath(path.dirname(__file__))
@@ -25,33 +20,12 @@ class Config:
 
     # Cognito
     AWS_REGION = environ["AWS_REGION"]
-    AWS_DEFAULT_REGION = AWS_REGION
     AWS_COGNITO_DOMAIN = environ["AWS_COGNITO_DOMAIN"]
     AWS_COGNITO_USER_POOL_ID = environ["AWS_COGNITO_USER_POOL_ID"]
     AWS_COGNITO_USER_POOL_CLIENT_ID = environ["AWS_COGNITO_USER_POOL_CLIENT_ID"]
     AWS_COGNITO_USER_POOL_CLIENT_SECRET = environ["AWS_COGNITO_USER_POOL_CLIENT_SECRET"]
     AWS_COGNITO_REDIRECT_URL = path.join(FLASK_SITE_URL, "postlogin")
-
-    # JWT
-    FLASK_JWT_SECRET_KEY = environ.get("FLASK_JWT_SECRET_KEY", urandom(32))
-    JWT_ALGORITHM = "RS256"
-
-    # CSRF protection
-    JWT_TOKEN_LOCATION = ["cookies"]
-    JWT_COOKIE_CSRF_PROTECT = False  # TODO is this required?
-    JWT_COOKIE_SECURE = True
-
-    try:
-        JWT_PUBLIC_KEY = RSAAlgorithm.from_jwk(
-            get_cognito_public_keys(
-                region=AWS_REGION,
-                pool_id=AWS_COGNITO_USER_POOL_ID,
-            )
-        )
-    except InvalidKeyError:
-        logging.warning(
-            "Could not access Cognito public keys. Secure routes may not be accessible"
-        )
+    AWS_COGNITO_LOGOUT_URL = path.join(FLASK_SITE_URL, "postlogout")
 
     # Static Assets
     STATIC_FOLDER = "static"
